6fab542533b424b57703b12d2186a73901f64ae4
Mike Eng <mike.eng@adhocteam.us>
2020-09-15 23:02:37 -0400

    overwriting style of neve theme with neve child theme sass

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -4,12 +4,12 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'neve-style'; // loaded in front_end.php inside the neve theme folder
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
+    //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array( $parent_style),
         wp_get_theme()->get('Version')
     );
 }
