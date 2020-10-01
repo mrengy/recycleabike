7979993b321123b13cd8f1b598a4c51b72743a1d
Joe Stein <joe@splitwise.com>
2020-10-01 16:22:57 -0500

    Add hard-coded front-page caption

--- wp-content/themes/neve-child/page-templates/template-frontpage-covid.php
+++ wp-content/themes/neve-child/page-templates/template-frontpage-covid.php
@@ -6,8 +6,14 @@
 get_header();
 ?>
 <div class="covid-frontpage">
-	<div class="wrapper">
-		<div class="featured-image" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
+	<div class="content-wrapper">
+		<div class="relative">
+			<div class="featured-image" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
+			<div class="caption">
+				<p>Recycle-a-Bike is a non-profit community bike shop in Providence, RI.</p>
+				<p>Our mission is educating, empowering, and building community by connecting people and bicycles.</p>
+			</div>
+		</div>
 		<div class="container">
 	<?php
 		while ( have_posts() ) {
