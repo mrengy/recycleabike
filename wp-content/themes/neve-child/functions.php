49853d1c33a2c0c378bde07cf659207473565fd5
Joe Stein <joe@splitwise.com>
2020-10-07 17:37:36 -0400

    Compress SASS output

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -8,7 +8,7 @@ function my_theme_enqueue_styles() {
 
     //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/style.min.css',
         array( $parent_style),
         wp_get_theme()->get('Version')
     );
