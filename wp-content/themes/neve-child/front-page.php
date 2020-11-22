51fae8e8e69417a387d29f72f9edee999b82d308
Mike Eng <mike.eng@adhocteam.us>
2020-11-21 19:03:24 -0500

    added row div

--- wp-content/themes/neve-child/front-page.php
+++ wp-content/themes/neve-child/front-page.php
@@ -18,45 +18,47 @@ get_header();
 				?>
 			</div>
 		</div>
-		<?php do_action( 'neve_do_sidebar', 'single-page', 'left' ); ?>
-		<div class="nv-content-wrap col">
-			<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'template-parts/content', 'pagebuilder' );
-				}
-			?>
-			<div id="recent-blog-posts">
-			<h1>Recent Blog Posts</h1>
-			<ul>
+		<div class="row">
+			<?php do_action( 'neve_do_sidebar', 'single-page', 'left' ); ?>
+			<div class="nv-content-wrap col">
 				<?php
-				// Define our WP Query Parameters
-				$the_query = new WP_Query( 'posts_per_page=3' ); ?>
-
-
-				<?php
-				// Start our WP Query
-				while ($the_query -> have_posts()) : $the_query -> the_post();
-				// Display the Post Title with Hyperlink
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'template-parts/content', 'pagebuilder' );
+					}
 				?>
-				<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+				<div id="recent-blog-posts">
+				<h1>Recent Blog Posts</h1>
+				<ul>
+					<?php
+					// Define our WP Query Parameters
+					$the_query = new WP_Query( 'posts_per_page=3' ); ?>
 
-				<span class="nv-meta-list">
-					<li class="meta date posted-on"><?php the_date(); ?></li>
-				</span>
 
-				<li><?php
-				// Display the Post Excerpt
-				the_excerpt(); ?></li>
+					<?php
+					// Start our WP Query
+					while ($the_query -> have_posts()) : $the_query -> the_post();
+					// Display the Post Title with Hyperlink
+					?>
+					<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
 
-				<?php
-				// Repeat the process and reset once it hits the limit
-				endwhile;
-				wp_reset_postdata();
-				?>
-			</ul>
+					<span class="nv-meta-list">
+						<li class="meta date posted-on"><?php the_date(); ?></li>
+					</span>
+
+					<li><?php
+					// Display the Post Excerpt
+					the_excerpt(); ?></li>
+
+					<?php
+					// Repeat the process and reset once it hits the limit
+					endwhile;
+					wp_reset_postdata();
+					?>
+				</ul>
+			</div>
+			</div>
+			<?php do_action( 'neve_do_sidebar', 'single-page', 'right' ); ?>
 		</div>
-		</div>
-		<?php do_action( 'neve_do_sidebar', 'single-page', 'right' ); ?>
 	</div>
 <?php get_footer();	?>
