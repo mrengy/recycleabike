dce3bffce1fbbc53778d86a5ae2651142e0074e5
Mike Eng <mike.eng@adhocteam.us>
2020-11-21 19:36:44 -0500

    moving sidebar out of col class

--- wp-content/themes/neve-child/front-page.php
+++ wp-content/themes/neve-child/front-page.php
@@ -7,7 +7,8 @@ get_header();
 ?>
 <div class="frontpage">
 	<div class="content-wrapper container single-page-container ">
-		<div id="featured-container">
+		<div class="row">
+			<div id="featured-container">
 			<div id="featured-image" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
 			<div id="featured-caption">
 				<?php
@@ -18,45 +19,46 @@ get_header();
 				?>
 			</div>
 		</div>
-		<div class="row">
 			<?php do_action( 'neve_do_sidebar', 'single-page', 'left' ); ?>
-			<div class="nv-content-wrap col">
-				<?php
-					while ( have_posts() ) {
-						the_post();
-						get_template_part( 'template-parts/content', 'pagebuilder' );
-					}
-				?>
-				<div id="recent-blog-posts">
-				<h1>Recent Blog Posts</h1>
-				<ul>
+			<div class="nv-single-page-wrap col">
+				<div class="nv-content-wrap">
 					<?php
-					// Define our WP Query Parameters
-					$the_query = new WP_Query( 'posts_per_page=3' ); ?>
-
-
-					<?php
-					// Start our WP Query
-					while ($the_query -> have_posts()) : $the_query -> the_post();
-					// Display the Post Title with Hyperlink
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'template-parts/content', 'pagebuilder' );
+						}
 					?>
-					<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+					<div id="recent-blog-posts">
+					<h1>Recent Blog Posts</h1>
+					<ul>
+						<?php
+						// Define our WP Query Parameters
+						$the_query = new WP_Query( 'posts_per_page=3' ); ?>
 
-					<span class="nv-meta-list">
-						<li class="meta date posted-on"><?php the_date(); ?></li>
-					</span>
 
-					<li><?php
-					// Display the Post Excerpt
-					the_excerpt(); ?></li>
+						<?php
+						// Start our WP Query
+						while ($the_query -> have_posts()) : $the_query -> the_post();
+						// Display the Post Title with Hyperlink
+						?>
+						<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
 
-					<?php
-					// Repeat the process and reset once it hits the limit
-					endwhile;
-					wp_reset_postdata();
-					?>
-				</ul>
-			</div>
+						<span class="nv-meta-list">
+							<li class="meta date posted-on"><?php the_date(); ?></li>
+						</span>
+
+						<li><?php
+						// Display the Post Excerpt
+						the_excerpt(); ?></li>
+
+						<?php
+						// Repeat the process and reset once it hits the limit
+						endwhile;
+						wp_reset_postdata();
+						?>
+					</ul>
+				</div>
+				</div>
 			</div>
 			<?php do_action( 'neve_do_sidebar', 'single-page', 'right' ); ?>
 		</div>
