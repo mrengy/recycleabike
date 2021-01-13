fa5c42001fac2ffe09d0be40feb643b294490941
Mike Eng <mike.eng@adhocteam.us>
2021-01-12 23:26:43 -0500

    midway on homepage grid layout

--- wp-content/themes/neve-child/front-page.php
+++ wp-content/themes/neve-child/front-page.php
@@ -21,8 +21,35 @@ get_header();
 				<div class="neve-child-nowrap" id="main-container">
 					<?php do_action( 'neve_do_sidebar', 'single-page', 'left' ); ?>
 					<div class="nv-single-page-wrap col-sm-8 col-12">
-						<div class="nv-content-wrap">
-							grid area
+						<div class="nv-content-wrap" id="homepage-grid-container">
+							<a class="homepage-grid-item col-12 col-sm-6" href="about" id="homepage-grid-about">
+								<div class="grid-name-background col-6">
+									<div class="grid-name">
+										About
+									</div>
+								</div>
+							</a>
+							<a class="homepage-grid-item col-12 col-sm-6" href="friends" id="homepage-grid-friends">
+								<div class="grid-name-background col-6">
+									<div class="grid-name">
+										Friends
+									</div>
+								</div>
+							</a>
+							<a class="homepage-grid-item col-12 col-sm-6" href="blog" id="homepage-grid-blog">
+								<div class="grid-name-background col-6">
+									<div class="grid-name">
+										Blog
+									</div>
+								</div>
+							</a>
+							<a class="homepage-grid-item col-12 col-sm-6" href="donate" id="homepage-grid-donate">
+								<div class="grid-name-background col-6">
+									<div class="grid-name">
+										Donate
+									</div>
+								</div>
+							</a>
 						</div>
 					</div>
 					<div class="nv-sidebar-wrap col-sm-4 col-12 nv-right blog-sidebar" id="page-content">
